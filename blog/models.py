1df61673ab8950a0d144655e449b96ed80246069
user4920 <dltjgus@duksung.ac.kr>
2022-10-06 14:07:36 +0900

    포스트 목록/상세 페이제에 대표이미지 출력하기

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True) #(_media/)blog/images/년/월/일
+    # %Y 2022, %y 22
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- myDjangoPrj/urls.py
+++ myDjangoPrj/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = [ #IP주소/
     path('admin/', admin.site.urls), #IP주소/admin/
     path('blog/', include('blog.urls')), #IP주소/blog/
     path('', include('single_pages.urls')), #IP주소/
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
