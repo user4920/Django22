756c083342a213ad60f3300cb78ae2765a93374e
user4920 <dltjgus@duksung.ac.kr>
2022-11-10 13:42:59 +0900

    포스트 수정 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [ #IP주소/blog
     path('', views.PostList.as_view()), #인덱스부분 클래스로 대체
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('create_post/', views.PostCreate.as_view()),
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('category/<str:slug>/', views.category_page), #IP주소/blog/category/slug/
     path('tag/<str:slug>/', views.tag_page), #IP주소/blog/tag/slug
 
